cc36679bb8be5e68e08283d8420e32715cbcb976
florida97 <weichen.lean@gmail.com>
2024-04-12 02:51:04 +0800

    direBoar

--- Crimson Woods/Assets/Scripts/Lean/Enemy/DireBoar/Dire Boar State Machine/DireBoar.cs
+++ Crimson Woods/Assets/Scripts/Lean/Enemy/DireBoar/Dire Boar State Machine/DireBoar.cs
@@ -310,7 +310,7 @@ public class DireBoar : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(1);
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(2);
         }
     }
 }
